a559d30293721a156bde46d984d332538aeb50db
poojasharma122 <design.avology@gmail.com>
2025-05-23 13:15:48 +0530

    zbfb

---
+++ js/custom.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $(".menu-toggle").click(function(){
+      $("body").addClass("toggle");
+      $(".closed-menu").click(function(){
+        $("body").removeClass("toggle");
+      });
+    });
+
+
+  });
+
+
+
